ecf8d7b40b66e4f947028ae519249efeeeaba0c2
onimuraaika <aika.onimura@gmail.com>
2021-03-22 22:16:09 +0900

    [Add]ネストしたif else文を使ってみる

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-let num = 11;
+let num =50;
 
-if (num % 2 == 0) {
-  window.alert(num + 'は偶数です');
+if (num >= 0) {
+  if (num % 5 == 0) {
+    window.alert(num + 'は5の倍数です');
+  } else {
+    window.alert(num + 'は5の倍数ではありません');
+  }
 } else {
-  window.alert(num + 'は奇数です')
-}
+  window.alert('0以上の場合のみ判定します');
+ }
