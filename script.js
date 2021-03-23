f58f02c12666ee5efd19e763b3fd6ead844ef26e
onimuraaika <aika.onimura@gmail.com>
2021-03-23 21:41:55 +0900

    [Start/Add]for文を使ってみる

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-let point = 3;
-
-switch (point) {
-  case 1:
-    window.alert('ざんねん・・・');
-    break;
-  case 2:
-    window.alert('おしい！');
-    break;
-  case 3:
-    window.alert('おめでとう！');
-    break;
-  default:
-    window.alert('1~3以外は無効だよ');
-    break;
+for (let i = 0; i < 10; i++) {
+  document.write(i + `<br>`);
 }
+
+//for文　0〜9まで10買い繰り返して、数字を書き出す処理
