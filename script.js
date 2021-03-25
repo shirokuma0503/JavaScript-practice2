5f280e1797512c39154bd5d8d5e9b4fa48c6b9bb
onimuraaika <aika.onimura@gmail.com>
2021-03-25 21:58:55 +0900

    [Add]ラベル付きbreak文を使ってみる

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-for (var i = 1; i <= 3; i++) {
+outside: for (var i = 1; i <= 3; i++) {
   for (var j = 1; j <= 3; j++) {
     if (i == 2) {
-      break;
+      break outside;
     }
     document.write(i + '*' + j + ' = ' + (i * j) + '<br>');
   }
 }
 
-//多重ループとbreak文を使う
+//ラベル付きbreak文を使う
