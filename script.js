5079b369ee7c66526250ce0aae0468a65b82b0fc
onimuraaika <aika.onimura@gmail.com>
2021-03-25 22:06:00 +0900

    [Add]ラベル付きcontinue文を使ってみる

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 outside: for (var i = 1; i <= 3; i++) {
-  for (var j = 1; j <= 3; j++) {
-    if (i == 2) {
-      break outside;
-    }
-    document.write(i + '*' + j + ' = ' + (i * j) + '<br>');
+  while (true) {
+    document.write(i + '<br>');
+    continue outside;
   }
 }
 
-//ラベル付きbreak文を使う
+//ラベル付きcontinue文を使う
