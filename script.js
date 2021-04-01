955ca6e98bee2e122ad73211817bcaee16ab7355
onimuraaika <aika.onimura@gmail.com>
2021-04-01 10:54:25 +0900

    [Add]for文で配列を取り出してみる

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var name1 = ['しろくま', 'くろくま', 'こぐま'];
-var ary = name1.splice(1, 2);
 
-document.write('ary = ' + ary + '<br>');
+for(var i = 0; i < name1.length; i++) {
+  document.write(name1[i] + 'さん！' + '<br>');
+}
+
 document.write('name1 = ' + name1 + '<br>');
