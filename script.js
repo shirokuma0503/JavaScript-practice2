33905efd8f78886df25b5eed443a5ebfbee228ae
onimuraaika <aika.onimura@gmail.com>
2021-04-01 10:47:37 +0900

    [Add]spliceメソッドで要素を一部取り出してみる

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var name1 = ['しろくま', 'くろくま', 'こぐま'];
-var ary = name1.slice(1, 3);
+var ary = name1.splice(1, 2);
 
 document.write('ary = ' + ary + '<br>');
+document.write('name1 = ' + name1 + '<br>');
