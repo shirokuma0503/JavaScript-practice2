80776b0300b4f90d5556ce0700256a50fc6ee997
onimuraaika <aika.onimura@gmail.com>
2021-04-19 15:42:46 +0900

    fizzbuzzの練習

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const ageToType = (age) => {
-  if(age < 3) {
-    return '赤ちゃん';
-  } else if(age < 18) {
-    return '大人';
-  } else if(age < 65) {
-    return 'お年寄り';
+const fizzBuzz = (x) => {
+  if(x % 15 == 0) {
+    return '15でわりきれる';
+  } else if(x % 3 === 0) {
+    return '3でわりきれる';
+  } else if(x % 5 === 0) {
+    return '5でわりきれる';
   }
 }
 
-const type = ageToType(10);
-document.write(type);
+for (let i = 1; i <= 100; i++) {
+  document.write(fizzBuzz(i));
+  document.write('<br/>');
+}
